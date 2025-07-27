10726353759c1944d19a05df6bce45d6bf41d53f
Piyush Yadav <koshijeager@gmail.com>
2025-07-27 22:07:52 +0530

    01:setup

---
+++ vibe/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+const Page  = ()=> { 
+  return (
+    <div className="font-bold text-rose-400">
+      Hello World
+      <Button variant="new">
+        Hello 
+      </Button>
+    </div>
+  )
+}
+
+
+export default Page; 
