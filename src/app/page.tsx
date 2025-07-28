26170a6b0eb14bc9ca7e934736aad55f5b4dd3b1
Piyush Yadav <koshijeager@gmail.com>
2025-07-28 20:22:13 +0530

    03:setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+// "use client"; 
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { caller, getQueryClient , trpc} from "@/trpc/server";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { Client } from "./Client";
+import { Suspense } from "react";
 
-const Page  = ()=> { 
+const Page  = async()=> { 
+  const data = await caller.CreateAi({text:"Piyush's Server"}); 
+  const queryClient = getQueryClient(); 
+  void queryClient.prefetchQuery(trpc.CreateAi.queryOptions({text : "Piyush's Prefetch"}))
+ 
+//   const trpc = useTRPC(); 
+//  const data = useQuery(trpc.CreateAi.queryOptions({text :"Piyush"})); 
   return (
-    <div className="font-bold text-rose-400">
-      Hello World
-      <Button variant="new">
-        Hello 
-      </Button>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Client/>
+      </Suspense>
+    </HydrationBoundary>
+
   )
 }
 
