a1590f200f2c543a5815d01b4bcd78410cc7bba1
Piyush Yadav <koshijeager@gmail.com>
2025-07-29 20:52:51 +0530

    0:5 Agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
  
 
 const Page  = ()=> { 
+  const [value, setValue] = useState("");
    const trpc = useTRPC(); 
    const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess :() =>{ 
@@ -21,7 +24,8 @@ const Page  = ()=> {
 //  const data = useQuery(trpc.CreateAi.queryOptions({text :"Piyush"})); 
   return (
 <div className="p-4 max-w-7xl mx-auto">
- <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({text : "Richa Yadav"})}>
+  <Input value={value} onChange={(e) => setValue(e.target.value)}/>
+ <Button disabled={invoke.isPending} onClick={()=> invoke.mutate({value : value})}>
   Invoke Background Job
  </Button>
 </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,20 @@
 import { inngest } from "./client";
+import {openai, gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    //anaylzing stuff 
-    await step.sleep("wait-a-moment", "30s");
-//thinking step
-     await step.sleep("wait-a-moment", "10s");
-//summary staghe
-      await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.name}!` };
+    const CodeAgent = createAgent({
+  name: "CodeAgent",
+  system: "You are an expert next.js developer, you write readble, maintable code. you write simple next.js & react snippets",
+  model: gemini({ model: "gemini-1.5-flash"}),
+});
+
+const { output } = await CodeAgent.run(
+  'Write the following snippit:' + event.data.value,
+);
+console.log(output);
+    return { output};
   },
 );
